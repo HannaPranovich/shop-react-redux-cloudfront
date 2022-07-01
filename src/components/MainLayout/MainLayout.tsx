0eda144e8af7c349a7de85656a3128d48a3b5c19
Hanna Pranovich <hanna.pranovich@gmail.com>
2022-07-01 22:45:27 +0400

    update endpoint to get product list

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 import Container from "@material-ui/core/Container";
 import Header from "components/MainLayout/components/Header";
@@ -10,7 +10,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        My Store
+        Christmas Tree Decorations
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -28,12 +28,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const MainLayout: React.FC = ({children}) => {
+const MainLayout: React.FC = ({ children }) => {
   const classes = useStyles();
 
   return (
     <>
-      <Header/>
+      <Header />
       <main>
         <Container className={classes.container} maxWidth="md">
           {children!}
@@ -43,7 +43,7 @@ const MainLayout: React.FC = ({children}) => {
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
           Thank you for your purchase!
         </Typography>
-        <Copyright/>
+        <Copyright />
       </footer>
     </>
   );
--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  bff: 'https://f7smai4huj.execute-api.eu-west-1.amazonaws.com',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
 
